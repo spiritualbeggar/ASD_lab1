8e1a39871a39260eb0e7266627f41f77859a3666
Daria <darigrecha@gmail.com>
2024-09-21 11:03:34 +0300

    Реализация АТД

--- lab1.py
+++ lab1.py
@@ -1,13 +1,29 @@
 from collections import deque
 import random
 
+# Абстрактный тип данных для событий
+class EventQueue:
+    def __init__(self):
+        self.queue = deque()
+
+    def add_event(self, event_type, time):
+        # Вставляем событие в правильное место в очереди
+        for i, (event_time, _) in enumerate(self.queue):
+            if time < event_time:
+                self.queue.insert(i, (time, event_type))
+                return
+        self.queue.append((time, event_type))
+
+    def get_next_event(self):
+        return self.queue.popleft() if self.queue else None
+
 # Параметры модели
 arrival_rate = 1.0  # Средняя интенсивность прихода клиентов (клиенты в единицу времени)
 service_rate = 1.5  # Средняя интенсивность обслуживания (клиенты в единицу времени)
 simulation_time = 100.0  # Время моделирования
 
 # Инициализация
-event_queue = deque()  # Очередь событий (DEQ)
+event_queue = EventQueue()  # Очередь событий (АТД)
 current_time = 0.0
 num_lost = 0  # Количество потерянных клиентов
 num_served = 0  # Количество обслуженных клиентов
@@ -21,23 +37,16 @@ def generate_arrival_time():
 def generate_service_time():
     return random.expovariate(service_rate)
 
-# Добавление события в очередь с приоритетом
-def add_event(event_type, time):
-    # Вставляем событие в правильное место в очереди
-    for i, (event_time, _) in enumerate(event_queue):
-        if time < event_time:
-            event_queue.insert(i, (time, event_type))
-            return
-    event_queue.append((time, event_type))
-
 # Инициализация первого события прихода клиента
 next_arrival_time = generate_arrival_time()
-add_event('arrival', next_arrival_time)
+event_queue.add_event('arrival', next_arrival_time)
 
 # Основной цикл моделирования
 while current_time < simulation_time:
     # Получаем следующее событие
-    event_time, event_type = event_queue.popleft()
+    event_time, event_type = event_queue.get_next_event()
+    if event_time is None:
+        break
     current_time = event_time
 
     if event_type == 'arrival':
@@ -45,7 +54,7 @@ while current_time < simulation_time:
         if not server_busy:
             # Сервер свободен, начинаем обслуживание
             service_time = generate_service_time()
-            add_event('departure', current_time + service_time)
+            event_queue.add_event('departure', current_time + service_time)
             server_busy = True
         else:
             # Сервер занят, клиент теряется
@@ -53,7 +62,7 @@ while current_time < simulation_time:
 
         # Планируем следующий приход клиента
         next_arrival_time = current_time + generate_arrival_time()
-        add_event('arrival', next_arrival_time)
+        event_queue.add_event('arrival', next_arrival_time)
 
     elif event_type == 'departure':
         # Окончание обслуживания клиента
